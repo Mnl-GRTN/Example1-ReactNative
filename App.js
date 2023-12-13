872e76546e4889ee550d6eef84ac084a38814a73
Mnl-GRTN <anotix13@gmail.com>
2023-12-13 15:32:10 +0100

    ScrollView

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, TextInput, KeyboardAvoidingView} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TextInput, KeyboardAvoidingView, ScrollView} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container} behavior='padding'>
-      <StatusBar style='auto'/>
-      <View style={{flex:1, backgroundColor: '#788b28', display:'flex',}}>
-        <Text style={styles.bartitle}>Example 1</Text>
-      </View>
-      <Image source={require('./assets/icon.png')} style={styles.images}/>
-      <View style={styles.buttonlayout}>
-        <View style={{flex:1, display:'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center',}}>
-          <Button title="Button" color="#788b28"/>
-          <Button title="Button 2" color="#788b28"/>
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior='padding'>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <View style={{ backgroundColor: '#788b28', padding: 20 }}>
+            <Text style={styles.bartitle}>Example 1</Text>
+          </View>
+          <View style={{ alignItems: 'center' }}>
+            <Image source={require('./assets/icon.png')} style={styles.images} />
+            <View style={styles.buttonlayout}>
+              <View style={styles.buttonRow}>
+                <Button title="Button 1" color="#788b28" />
+                <Button title="Button 2" color="#788b28" />
+              </View>
+              <View style={styles.buttonRow}>
+                <Button title="Button 3" color="#788b28" />
+                <Button title="Button 4" color="#788b28" />
+              </View>
+            </View>
+            <View style={{width:'100%', flexDirection: 'row', justifyContent: 'flex-start', marginTop: 20}}>
+              <Text style={{ paddingLeft: 16 }}>E-mail : </Text>
+              <View style={{ flex: 1, alignItems:'center' }}>
+                <TextInput
+                  placeholder='Enter a valid e-mail'
+                  style={{
+                    width:'70%',
+                    borderBottomColor: '#788b28',
+                    borderBottomWidth: 1,
+                    color: 'grey',}}/>
+              </View>
+            </View>
+          </View>
         </View>
-        <View style={{flex:1, display:'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center',}}>
-          <Button title="Button 3" color="#788b28"/>
-          <Button title="Button 4" color="#788b28"/>
-        </View>
-      </View>
-      <View style={{flex:4, backgroundColor: 'white'}}>
-        <KeyboardAvoidingView behavior='height' style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
-          <Text style={{paddingTop: 4}}>E-mail : </Text>
-          <TextInput placeholder='Enter a valid e-mail'></TextInput>
-        </KeyboardAvoidingView>
-      </View>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -34,24 +45,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-
-  images:{
-    flex:2,
-    aspectRatio: 1,
+  images: {
+    width: 200,
+    height: 200,
     alignSelf: 'center',
-    margin : 10,
+    margin: 20,
   },
-
-  bartitle:{
-    position: 'absolute',
-    bottom: "15%",
-    left: "5%",
+  bartitle: {
     color: 'white',
     fontSize: 20,
     fontWeight: 'bold',
   },
-
-  buttonlayout:{
-    flex: 2,
+  buttonlayout: {
+    marginTop: 20,
+    width: '100%',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginBottom: 10,
   },
 });
+
